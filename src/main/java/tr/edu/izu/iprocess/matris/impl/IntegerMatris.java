df6baadc53df7dc0c80b2747c6b8f3c12daee302
yahyas <yahyas@yahyas-Lenovo-U310>
2018-03-12 15:25:58 +0300

    Transpose

--- src/main/java/tr/edu/izu/iprocess/matris/impl/IntegerMatris.java
+++ src/main/java/tr/edu/izu/iprocess/matris/impl/IntegerMatris.java
@@ -6,8 +6,6 @@ import tr.edu.izu.iprocess.matris.exception.NotInitializedMatrixException;
 
 public class IntegerMatris extends AbstractMatris<Integer> {
 
-	private Integer[][] matrisDatas;
-
 	public IntegerMatris(int aRow, int aCol) {
 		this(aRow, aCol, 0);
 	}
@@ -18,7 +16,7 @@ public class IntegerMatris extends AbstractMatris<Integer> {
 
 	@Override
 	protected Integer[][] initMatris(Integer defaultValue) {
-		matrisDatas = new Integer[this.getRowsCount()][this.getColumnsCount()];
+		Integer[][] matrisDatas = new Integer[this.getRowsCount()][this.getColumnsCount()];
 		for (int row = 0; row < this.getRowsCount(); row++)
 			for (int col = 0; col < this.getColumnsCount(); col++)
 				matrisDatas[row][col] = defaultValue;
@@ -28,47 +26,47 @@ public class IntegerMatris extends AbstractMatris<Integer> {
 	}
 
 	public void sumWithScalar(Integer scalar) throws Exception {
-		if (matrisDatas == null)
+		if (getDatas() == null)
 			throw new NotInitializedMatrixException();
 		for (int row = 0; row < this.getRowsCount(); row++)
 			for (int col = 0; col < this.getColumnsCount(); col++)
-				matrisDatas[row][col] = matrisDatas[row][col] + scalar;
+				getDatas()[row][col] = getDatas()[row][col] + scalar;
 
 	}
 
 	public void multiplyWithScalar(Integer scalar) throws Exception {
-		if (matrisDatas == null)
+		if (getDatas() == null)
 			throw new NotInitializedMatrixException();
 		for (int row = 0; row < this.getRowsCount(); row++)
 			for (int col = 0; col < this.getColumnsCount(); col++)
-				matrisDatas[row][col] = matrisDatas[row][col] * scalar;
+				getDatas()[row][col] = getDatas()[row][col] * scalar;
 	}
 
 	public void substractWithScalar(Integer scalar) throws Exception {
-		if (matrisDatas == null)
+		if (getDatas() == null)
 			throw new NotInitializedMatrixException();
 		for (int row = 0; row < this.getRowsCount(); row++)
 			for (int col = 0; col < this.getColumnsCount(); col++)
-				matrisDatas[row][col] = matrisDatas[row][col] - scalar;
+				getDatas()[row][col] = getDatas()[row][col] - scalar;
 	}
 
 	public void divideWithScalar(Integer scalar) throws Exception {
-		if (matrisDatas == null)
+		if (getDatas() == null)
 			throw new NotInitializedMatrixException();
 		for (int row = 0; row < this.getRowsCount(); row++)
 			for (int col = 0; col < this.getColumnsCount(); col++)
-				matrisDatas[row][col] = matrisDatas[row][col] / scalar;
+				getDatas()[row][col] = getDatas()[row][col] / scalar;
 	}
 
 	public void transpose() throws Exception {
-		if (matrisDatas == null)
+		if (getDatas() == null)
 			throw new NotInitializedMatrixException();
+		Integer[][] matrisDatas = new Integer[this.getRowsCount()][this.getColumnsCount()];
 		for (int row = 0; row < this.getRowsCount(); row++)
-			for (int col = 0; col < this.getColumnsCount(); col++) {
-				Integer temp = matrisDatas[row][col];
-				matrisDatas[row][col] = matrisDatas[col][row];
-				matrisDatas[col][row] = temp;
-			}
+			for (int col = 0; col < this.getColumnsCount(); col++)
+				matrisDatas[row][col] = getDatas()[col][row];
+		setDatas(matrisDatas);
+
 	}
 
 	public AbstractMatris<Integer> multiply(AbstractMatris<Integer> otherMatris) throws Exception {
